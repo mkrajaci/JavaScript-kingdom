80fee41ec1c4f9d288a8122d7b18b0981daf4c1b
Mario <krajacic.mario@gmail.com>
2019-06-11 06:21:34 +0200

    Implementing game logic on hold button

--- 4-JS-Pig-Game-In-Browser-and-DOM-manipulation-and-events/app.js
+++ 4-JS-Pig-Game-In-Browser-and-DOM-manipulation-and-events/app.js
@@ -61,3 +61,13 @@ document.querySelector('.btn-roll').addEventListener('click', function() {
     document.querySelector('.dice').style.display = 'none';
   }
 });
+
+document.querySelector('.btn-hold').addEventListener('click', function() {
+  // Add current score to global score
+  scores[activePlayer] += roundScore;
+
+  // Update the UI
+  document.querySelector('#score-' + activePlayer).textContent =
+    scores[activePlayer];
+  // Check if player won the game
+});
