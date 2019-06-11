08defdb1087b879fa40c1fb4edfef6c99c9de9d3
Mario <krajacic.mario@gmail.com>
2019-06-11 06:09:57 +0200

    Class name manipulation

--- 4-JS-Pig-Game-In-Browser-and-DOM-manipulation-and-events/app.js
+++ 4-JS-Pig-Game-In-Browser-and-DOM-manipulation-and-events/app.js
@@ -12,7 +12,7 @@ GAME RULES:
 var scores, roundScore, activePlayer;
 scores = [0, 0];
 roundScore = 0;
-activePlayer = 1;
+activePlayer = 0;
 
 // Setter - set values of  text or element in html
 //document.querySelector('#current-' + activePlayer).textContent = dice;
@@ -25,21 +25,39 @@ activePlayer = 1;
 // CSS manipulation - hideing dice
 document.querySelector('.dice').style.display = 'none';
 
-document.getElementById('score-0').textContent = '0'
-document.getElementById('score-1').textContent = '0'
-document.getElementById('current-0').textContent = '0'
-document.getElementById('current-1').textContent = '0'
-
+document.getElementById('score-0').textContent = '0';
+document.getElementById('score-1').textContent = '0';
+document.getElementById('current-0').textContent = '0';
+document.getElementById('current-1').textContent = '0';
 
 // Anonymus function
 document.querySelector('.btn-roll').addEventListener('click', function() {
-    // 1. Random number
-    dice = Math.floor(Math.random() * 6) + 1;
+  // 1. Random number
+  dice = Math.floor(Math.random() * 6) + 1;
 
-    // 2. Display result
-    var diceDOM = document.querySelector('.dice')
-    diceDOM.style.display = 'block';
-    diceDOM.src = 'dice-' + dice + '.png'
+  // 2. Display result
+  var diceDOM = document.querySelector('.dice');
+  diceDOM.style.display = 'block';
+  diceDOM.src = 'dice-' + dice + '.png';
 
-    // 3. Update the round score if rolled numbet was not a 1
+  // 3. Update the round score if rolled numbet was not a 1
+  if (dice != 1) {
+    // Add score
+    roundScore += dice;
+    document.querySelector('#current-' + activePlayer).textContent = roundScore;
+  } else {
+    // Next player
+    // Ternarni operator
+    activePlayer === 0 ? (activePlayer = 1) : (activePlayer = 0);
+    roundScore = 0;
+    document.getElementById('current-0').textContent = '0';
+    document.getElementById('current-1').textContent = '0';
+    // Manipulacija imenom klase kroz JS
+    // document.querySelector('.player-0-panel').classList.remove('active');
+    // document.querySelector('.player-1-panel').classList.add('active');
+    document.querySelector('.player-0-panel').classList.toggle('active');
+    document.querySelector('.player-1-panel').classList.toggle('active');
+
+    document.querySelector('.dice').style.display = 'none';
+  }
 });
