dea30d98bda162f58d420fc86059813e6f8ec7d6
Mario <krajacic.mario@gmail.com>
2019-06-15 10:04:20 +0200

    Object.create

--- 5-JS-Objects-and-functions/script.js
+++ 5-JS-Objects-and-functions/script.js
@@ -1,3 +1,4 @@
+/*
 var mario = {
   name: 'Mario',
   yearOfBirth: 1980,
@@ -30,7 +31,7 @@ var Osoba = function(name, yearOfBirth, job) {
   };
 };
 */
-
+/*
 var mario = new Osoba('Mario', 1980, 'developer');
 var marija = new Osoba('Marija', 1981, 'knjigovodja');
 var ivan = new Osoba('Ivan', 2011, 'ucenik');
@@ -40,3 +41,23 @@ ivan.calculateAge();
 console.log(mario.lastName);
 console.log(marija.lastName);
 console.log(ivan.lastName);
+*/
+
+// Object.create
+
+var personProto = {
+  calculateAge: function() {
+    console.log(2019 - this.yearOfBirth);
+  }
+};
+
+var mario = Object.create(personProto);
+mario.name = 'Mario';
+mario.yearOfBirth = 1980;
+mario.job = 'developer';
+
+var marija = Object.create(personProto, {
+  name: { value: 'Marija' },
+  yearOfBirth: { value: 1981 },
+  job: { value: 'designer' }
+});
