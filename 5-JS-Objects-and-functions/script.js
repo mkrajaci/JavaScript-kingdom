7aa3c27d83f2f9c504c4a57a9eadfa89a4a0be24
Mario <krajacic.mario@gmail.com>
2019-06-18 06:18:48 +0200

    Closures

--- 5-JS-Objects-and-functions/script.js
+++ 5-JS-Objects-and-functions/script.js
@@ -171,7 +171,24 @@ function game() {
 game();
 */
 // skriveno od vanjskog svijeta, privatnost varijable score je zagarantirana
+/*
 (function() {
   var score = Math.random() * 10;
   console.log(score >= 5);
 })();
+*/
+
+// Closures
+
+function retirement(retirementAge) {
+  var a = ' years left until retirement.';
+  return function(yearOfBirth) {
+    var age = 2019 - yearOfBirth;
+    console.log(retirementAge - age + a);
+  };
+}
+
+var retirementUs = retirement(66);
+
+retirementUs(1980);
+retirement(66)(1980);
