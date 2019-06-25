aa8e6f47d2053e250dcba4650807db9a6782e8c6
Mario <krajacic.mario@gmail.com>
2019-06-25 15:44:34 +0200

    Adding init method

--- 6-JS-Budget-App/app.js
+++ 6-JS-Budget-App/app.js
@@ -1,25 +1,55 @@
-var budgetController = (function() {
-  var x = 23;
+// Budget Controller
+var budgetController = (function() {})();
 
-  var add = function(a) {
-    return x + a;
+// IU Controller
+var UIController = (function() {
+  var DOMstrings = {
+    inputType: '.add__type',
+    inputDescription: '.add__description',
+    inputValue: '.add__value',
+    inputBtn: '.add__btn'
   };
   return {
-    publicTest: function(b) {
-      return add(b);
+    getInput: function() {
+      return {
+        type: document.querySelector(DOMstrings.inputType).value, // Biti ce inc ili exp
+        description: document.querySelector(DOMstrings.inputDescription).value,
+        value: document.querySelector(DOMstrings.inputValue).value
+      };
+    },
+    getDOMstrings: function() {
+      return DOMstrings;
     }
   };
 })();
 
-var UIController = (function() {
-  // some code
-})();
-
+// Global App Controller
 var controller = (function(budgetCtrl, UICtrl) {
-  var z = budgetCtrl.publicTest(5);
+  var setupEventLiteners = function() {
+    var DOM = UICtrl.getDOMstrings();
+    document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
+    document.addEventListener('keypress', function(event) {
+      // Kada korisnik pritisne enter ili bilo koji drugi key
+      // Kada korisnik pritisne enter - to znam prema key kocu kojeg ima svaka tipka
+      if (event.keyCode === 13 || event.which === 13) {
+        ctrlAddItem();
+      }
+    });
+  };
+  var ctrlAddItem = function() {
+    // 1. Get the field input data
+    var input = UICtrl.getInput();
+    // 2. Add the item to the budget controller
+    // 3. Add the item to the UI
+    // 4. Calculate the budget
+    // 5. Display the budget on UI
+  };
   return {
-    anotherPublic: function() {
-      console.log(z);
+    init: function() {
+      console.log('Application has started.');
+      setupEventLiteners();
     }
   };
 })(budgetController, UIController);
+
+controller.init();
